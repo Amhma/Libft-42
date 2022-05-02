c2fb36b283f97ddd12ef6cab5707631b9b7b2868
Amir MAHLA <amahla@e1r3p20.42.fr>
2022-05-02 14:01:17 +0200

    modify ft_substr

--- Libft_42/ft_substr.c
+++ Libft_42/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: amahla <amahla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 12:37:15 by amahla            #+#    #+#             */
-/*   Updated: 2022/05/02 12:37:16 by amahla           ###   ########.fr       */
+/*   Updated: 2022/05/02 13:51:56 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	len_s = 0;
 	while (s && *(s + len_s))
 		len_s++;
-	if (len > len_s)
-		len = len_s;
+	if (start > len_s)
+		len = 0;
+	else if (len > len_s - start)
+		len = len_s - start;
 	dst = malloc((len + 1) * sizeof(char));
 	if (!s && !dst)
 		return (NULL);
