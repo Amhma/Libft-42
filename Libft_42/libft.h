b6c1ea9fbec9319378d65f93b771b179652e9151
Amir MAHLA <amahla@e2r10p6.clusters.42paris.fr>
2022-05-03 12:05:36 +0200

    Final Libft

--- Libft_42/libft.h
+++ Libft_42/libft.h
@@ -6,7 +6,7 @@
 /*   By: amahla <amahla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 12:37:51 by amahla            #+#    #+#             */
-/*   Updated: 2022/05/03 09:37:05 by amahla           ###   ########.fr       */
+/*   Updated: 2022/05/03 12:01:58 by amahla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <stdint.h>
 
 typedef struct s_list
 {
@@ -24,9 +23,9 @@ typedef struct s_list
 }					t_list;
 
 /* string.h */
-void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memset(void *s, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
@@ -35,8 +34,8 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-void	*ft_memmove(void *dst, const void *src, size_t len);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+void	*ft_memmove(void *dest, const void *src, size_t len);
 
 /* ctype.h */
 int		ft_isalpha(int c);
@@ -48,9 +47,9 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 
 /* stdlib.h */
-int		ft_atoi(const char *str);
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_strdup(const char *s1);
+int		ft_atoi(const char *nptr);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strdup(const char *s);
 
 /* others */
 char	*ft_strjoin(char const *s1, char const *s2);
